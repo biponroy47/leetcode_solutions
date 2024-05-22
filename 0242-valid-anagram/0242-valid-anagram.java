7e00ad5dfe9508e50713a8edf957554a26b13536
Bipon Roy <68929730+biponroy47@users.noreply.github.com>
2024-05-22 10:14:38 -0400

    Time: 6 ms (44.61%), Space: 42.9 MB (82.49%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        int [] chars = new int [26];
+        
+        for(int i = 0; i < s.length(); i++){
+            chars[(int) s.charAt(i) - 'a']++;
+            chars[(int) t.charAt(i) - 'a']--;
+        }
+        
+        for(int i = 0; i < chars.length; i++){
+            if(chars[i] != 0) return false;
+        }
+        return true;
+    }
+}
