d3767aecd325874ca6b0f88eafac3285ccaf7376
Bipon Roy <68929730+biponroy47@users.noreply.github.com>
2024-05-22 10:23:55 -0400

    Time: 2 ms (97.76%), Space: 43 MB (77.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
+        char [] sc = s.toCharArray();
+        char [] tc = t.toCharArray();
         int [] chars = new int [26];
-        for(int i = 0; i < s.length(); i++){
-            chars[(int) s.charAt(i) - 'a']++;
-            chars[(int) t.charAt(i) - 'a']--;
-        }
+        for(char c : sc) chars[c - 'a']++;
+        for(char c : tc) chars[c - 'a']--;
         for(int i : chars) if(i != 0) return false;
         return true;
     }
