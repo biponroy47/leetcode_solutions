0a4a9c9f2af7c9cb47c834b2642a1ffb3e92a067
Bipon Roy <68929730+biponroy47@users.noreply.github.com>
2024-05-22 10:26:31 -0400

    Time: 2 ms (97.76%), Space: 43.2 MB (69.33%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,8 +4,8 @@ class Solution {
         char [] sc = s.toCharArray();
         char [] tc = t.toCharArray();
         int [] chars = new int [26];
-        for(char c : sc) chars[c - 'a']++;
-        for(char c : tc) chars[c - 'a']--;
+        for(char c : sc) chars[c - 97]++;
+        for(char c : tc) chars[c - 97]--;
         for(int i : chars) if(i != 0) return false;
         return true;
     }
