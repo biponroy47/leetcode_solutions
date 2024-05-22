3371c193f90d7baff90c66b83113396517435889
Bipon Roy <68929730+biponroy47@users.noreply.github.com>
2024-05-22 14:43:05 -0400

    Time: 16 ms (5.50%), Space: 57.4 MB (5.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,25 +1,16 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int [] res = new int [nums.length];
-        int [] lefts = new int [nums.length];
-        int [] rights = new int [nums.length];
         int left = 1, right = 1;
-        
         for(int i = 0; i < nums.length; i++){
-            lefts[i] = left * nums[i];
-            left = lefts[i];
+            res[i] = left;
+            left *= nums[i];
         }
+        System.out.println(Arrays.toString(res));
         for(int i = nums.length - 1; i >= 0; i--){
-            rights[i] = right * nums[i];
-            right = rights[i];
+            res[i] *= right; 
+            right *= nums[i];
         }
-        
-        res[0] = rights[1];
-        res[nums.length - 1] = lefts[nums.length - 2];
-        for(int i = 1; i < nums.length - 1; i++){
-            res[i] = lefts[i - 1] * rights[i + 1];
-        }
-        
         return res;
     }
 }
