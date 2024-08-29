b3fe690d1582a1e650d6b67ca39d74a1026a3ab4
Bipon Roy <68929730+biponroy47@users.noreply.github.com>
2024-08-28 21:39:22 -0400

    Time: 29 ms (71.8%), Space: 51.5 MB (52.01%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,16 +1,13 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        //System.out.println(Arrays.toString(nums));
         List<List<Integer>> list = new ArrayList<>();
-
         for(int i = nums.length - 1; i > 1; i--){
             int target = 0 - nums[i];
             int left = 0;
             int right = i - 1;
 
             while(left < right){
-                //System.out.println("l: " + nums[left] + ", r: " + nums[right] + ", target: " + target);
                 if(nums[left] + nums[right] == target){
                     List<Integer> set = List.of(nums[left], nums[right], nums[i]);
                     list.add(set);
@@ -29,7 +26,7 @@ class Solution {
                 else if(nums[left] + nums[right] < target){
                     left++;
                 }
-                else if(nums[left] + nums[right] > target){
+                else {
                     right--;
                 }
             }
