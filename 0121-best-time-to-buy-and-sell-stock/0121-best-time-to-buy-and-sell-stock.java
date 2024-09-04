b299e4a218a38317993b7ff546dafad49b7b436e
Bipon Roy <68929730+biponroy47@users.noreply.github.com>
2024-09-04 16:10:26 -0400

    Time: 1 ms (99.79%), Space: 62 MB (18.13%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,25 +1,26 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices.length == 1) return 0;
-
-        int l = 0;
-        int r = 1;
-        int profit = 0;
-        while(r < prices.length){
-            int low = prices[l];
-            int high = prices[r];
-            if(high > low){
-                int temp = high - low;
-                if(temp > profit){
-                    profit = temp;
-                }
-                r++;
-            }
-            else{
-                l = r;
-                r++;
-            }
+        if(prices.length == 1) {
+            return 0;
         }
-        return profit;
+        else{ 
+            int l = 0;
+            int r = 1;
+            int profit = 0;
+            while(r < prices.length){
+                if(prices[r] > prices[l]){
+                    int temp = prices[r] - prices[l];
+                    if(temp > profit){
+                        profit = temp;
+                    }
+                    r++;
+                }
+                else{
+                    l = r;
+                    r++;
+                }
+            }
+            return profit;
+        }
     }
 }
