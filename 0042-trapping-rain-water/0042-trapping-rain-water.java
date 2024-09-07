80626072fde16ecb164a33742fac21aceb724fbd
Bipon Roy <68929730+biponroy47@users.noreply.github.com>
2024-09-07 18:10:17 -0400

    Time: 0 ms (100%), Space: 46.4 MB (38.3%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,31 +1,31 @@
 class Solution {
     public int trap(int[] height) {
-        int [] maxLeft = new int[height.length];
-        int [] maxRight = new int[height.length];
-        int [] min = new int[height.length];
-        for(int i = 1; i < height.length; i++){
-            if(height[i - 1] > maxLeft[i - 1]){
-                maxLeft[i] = height[i - 1];
-            }
-            else {
-                maxLeft[i] = maxLeft[i - 1];
-            }
-        }
-        for(int i = height.length - 2; i >= 0; i--){
-            if(height[i + 1] > maxRight[i + 1]){
-                maxRight[i] = height[i + 1];
-            }
-            else {
-                maxRight[i] = maxRight[i + 1];
-            }
-        }
+        int lp = 0;
+        int maxl = height[0];
+        int rp = height.length - 1;
+        int maxr = height[rp];
         int water = 0;
-        for(int i = 0; i < height.length; i++){
-            min[i] = Math.min(maxLeft[i], maxRight[i]);
-            if(min[i] - height[i] > 0) {
-                water += min[i] - height[i];
+
+        while(lp != rp){
+            int min = Math.min(maxl, maxr); 
+            if(maxl <= maxr) {
+                if(min - height[++lp] > 0){
+                    water += min - height[lp];
+                }
+                if(height[lp] > maxl){
+                    maxl = height[lp];
+                }
+            }
+            else {
+                if(min - height[--rp] > 0){
+                    water += min - height[rp];
+                }
+                if(height[rp] > maxr){
+                    maxr = height[rp];
+                }
             }
         }
+
         return water;
     }
 }
