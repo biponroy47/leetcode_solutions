6c3cd58139eba5e5e64a67005ebe4be61a304ae6
Bipon Roy <68929730+biponroy47@users.noreply.github.com>
2024-09-09 04:50:28 -0400

    Time: 4 ms (96.02%), Space: 44.9 MB (35.51%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,65 @@
+class MinStack {
+
+    private List<Integer> list;
+    private int topIndex;
+    private List<Integer> mins;
+    private int minIndex;
+    private int min;
+
+    public MinStack() {
+        list = new ArrayList<>();
+        topIndex = 0;
+        mins = new ArrayList<>();
+        minIndex = 0;
+    }
+    
+    public void push(int val) {
+        if(list.isEmpty()){
+            list.add(val);
+            mins.add(val);
+        }
+        else{
+            list.add(val);
+            topIndex++;
+            if(val <= mins.get(minIndex)){
+                mins.add(val);
+                minIndex++;
+            }
+        }
+    }
+    
+    public void pop() {
+        if(list.isEmpty()){
+            return;
+        }
+        else if(list.get(topIndex).equals(mins.get(minIndex))){
+            list.remove(topIndex--);
+            mins.remove(minIndex--);
+        }
+        else {
+            list.remove(topIndex--);
+        }
+
+        if(list.isEmpty()){
+            topIndex = 0;
+            minIndex = 0;
+        }
+    }
+    
+    public int top() {
+        return list.get(topIndex);
+    }
+    
+    public int getMin() {
+        return mins.get(minIndex);
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
