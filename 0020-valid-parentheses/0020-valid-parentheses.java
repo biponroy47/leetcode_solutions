ef3e0aa426d56952007c628cb396ebcbdc4fba7b
Bipon Roy <68929730+biponroy47@users.noreply.github.com>
2024-09-08 20:25:23 -0400

    Time: 2 ms (52.15%), Space: 41.6 MB (37.1%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length() % 2 != 0) return false;
         Stack<Character> stack = new Stack<>();
         HashMap<Character, Character> map = new HashMap<>(3);
         map.put(')', '(');
@@ -9,7 +8,7 @@ class Solution {
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
             if(map.containsKey(c)){
-                if(!stack.isEmpty() && stack.peek() == map.get(c)){
+                if(!stack.isEmpty() && map.get(c).equals(stack.peek())){
                     stack.pop();
                 }
                 else{
