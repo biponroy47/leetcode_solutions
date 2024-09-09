78811a87dc47423f562d108f2d3993c7362ad12c
Bipon Roy <68929730+biponroy47@users.noreply.github.com>
2024-09-08 20:17:27 -0400

    Time: 2 ms (52.15%), Space: 41.3 MB (71.39%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isValid(String s) {
         if(s.length() % 2 != 0) return false;
         Stack<Character> stack = new Stack<>();
-        HashMap<Character, Character> map = new HashMap<>();
+        Map<Character, Character> map = new HashMap<>();
         map.put(')', '(');
         map.put('}', '{');
         map.put(']', '[');
